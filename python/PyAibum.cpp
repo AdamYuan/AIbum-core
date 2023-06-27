682881ab31f68710dcc84a2cbad5d705806a3fe5
AdamYuan <y13916619121@126.com>
2023-06-27 09:09:20 +0800

    wrap PyImage

--- python/PyAibum.cpp
+++ python/PyAibum.cpp
@@ -5,14 +5,30 @@
 
 namespace py = pybind11;
 
+class PyImage {
+private:
+	cv::Mat m_mat;
+
+public:
+	inline explicit PyImage(const char *filename) {
+		m_mat = cv::imread(filename, cv::IMREAD_COLOR);
+	}
+
+	inline explicit PyImage(const py::array_t<uint8_t> &image) {
+		py::buffer_info buf = image.request();
+		m_mat = cv::Mat(buf.shape[0], buf.shape[1], CV_8UC3, (unsigned char *) buf.ptr).clone();
+	}
+
+	inline const cv::Mat &GetMat() const { return m_mat; }
+};
+
 struct PyImageNet final : public aibum::ImageNet {
 	using aibum::ImageNet::ImageNet;
-	inline py::array_t<aibum::Tag> getTags(py::array_t<uint8_t> &image, unsigned int count) {
-		py::buffer_info buf = image.request();
-		cv::Mat mat(buf.shape[0], buf.shape[1], CV_8UC3, (unsigned char *)buf.ptr);
-		auto tags = GetTags(mat, count);
 
-		py::array_t<aibum::Tag> ret((ssize_t)tags.size());
+	inline py::array_t<aibum::Tag> getTags(const PyImage &image, unsigned int count) {
+		auto tags = GetTags(image.GetMat(), count);
+
+		py::array_t<aibum::Tag> ret((ssize_t) tags.size());
 		std::copy(tags.begin(), tags.end(), ret.mutable_data());
 		return ret;
 	}
@@ -20,26 +36,28 @@ struct PyImageNet final : public aibum::ImageNet {
 
 struct PyMTCNNFaceNet final : public aibum::MTCNNFaceNet {
 	using aibum::MTCNNFaceNet::MTCNNFaceNet;
-	inline py::array_t<aibum::Face> getFaces(py::array_t<uint8_t> &image) {
-		py::buffer_info buf = image.request();
-		cv::Mat mat(buf.shape[0], buf.shape[1], CV_8UC3, (unsigned char *)buf.ptr);
 
-		std::vector<aibum::Face> faces = GetFaces(mat);
+	inline py::array_t<aibum::Face> getFaces(const PyImage &image) {
+		std::vector<aibum::Face> faces = GetFaces(image.GetMat());
 
-		py::array_t<aibum::Face> ret((ssize_t)faces.size());
+		py::array_t<aibum::Face> ret((ssize_t) faces.size());
 		std::copy(faces.begin(), faces.end(), ret.mutable_data());
 		return ret;
 	}
 };
 
 PYBIND11_MODULE(pyaibum_core, m) {
+	py::class_<PyImage>(m, "Image")
+			.def(py::init<const char *>())
+			.def(py::init<const py::array_t<uint8_t> &>());
+
 	PYBIND11_NUMPY_DTYPE(aibum::Tag, index, score);
 	py::class_<PyImageNet>(m, "ImageNet")
-	    .def(py::init<const char *>())
-	    .def("getTags", &PyImageNet::getTags, py::return_value_policy::move);
+			.def(py::init<const char *>())
+			.def("getTags", &PyImageNet::getTags, py::return_value_policy::move);
 
 	PYBIND11_NUMPY_DTYPE(aibum::Face, x, y, size, feature);
 	py::class_<PyMTCNNFaceNet>(m, "MTCNNFaceNet")
-	    .def(py::init<const char *>())
-	    .def("getFaces", &PyMTCNNFaceNet::getFaces, py::return_value_policy::move);
+			.def(py::init<const char *>())
+			.def("getFaces", &PyMTCNNFaceNet::getFaces, py::return_value_policy::move);
 }
