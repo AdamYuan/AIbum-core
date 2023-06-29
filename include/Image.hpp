27f0514dfcffa78fc50a5dfdb988c2ecca6a381e
AdamYuan <y13916619121@126.com>
2023-06-29 23:49:48 +0800

    place Image in aibum namespace

--- include/Image.hpp
+++ include/Image.hpp
@@ -2,8 +2,12 @@
 
 #include <mat.h>
 
+namespace aibum {
+
 struct Image {
 	unsigned char *data;
 	int width, height;
 	ncnn::Mat::PixelType pixel_type;
 };
+
+} // namespace aibum
--- wasm/aibum_core_wasm.cpp
+++ wasm/aibum_core_wasm.cpp
@@ -18,11 +18,18 @@ public:
 	inline WASMImage() = default;
 	inline explicit WASMImage(const char *filename) { loadFromFile(filename); }
 	inline WASMImage(unsigned char *buffer, size_t size) { loadFromMemory(buffer, size); }
-	inline void loadFromMemory(const unsigned char *buffer, size_t size) {}
-	inline void loadFromFile(const char *filename) {}
+	inline void loadFromMemory(const unsigned char *buffer, size_t size) {
+		int comp;
+		m_data = stbi_load_from_memory(buffer, (int)size, &m_width, &m_height, &comp, 3);
+	}
+	inline void loadFromFile(const char *filename) {
+		int comp;
+		m_data = stbi_load(filename, &m_width, &m_height, &comp, 3);
+	}
 	inline void free() { stbi_image_free(m_data); }
+	inline bool valid() const { return m_data; }
 
-	inline Image GetImage() const { return {m_data, m_width, m_height, ncnn::Mat::PIXEL_RGB}; }
+	inline aibum::Image GetImage() const { return {m_data, m_width, m_height, ncnn::Mat::PIXEL_RGB}; }
 };
 
 class WASMImageNet {
@@ -35,6 +42,22 @@ public:
 		return m_object.GetTags(image.GetImage(), count);
 	}
 	inline void free() { m_object.~ImageNet(); }
+};
+
+class WASMFaceNet {
+private:
+	aibum::SCRFD m_detector;
+	aibum::FaceNet m_face_net;
+
+public:
+	inline WASMFaceNet() : m_detector(), m_face_net() {}
+	inline std::vector<aibum::Face> getFaces(const WASMImage &image) const {
+		return m_face_net.GetFaces(m_detector, image.GetImage());
+	}
+	inline void free() {
+		m_detector.~SCRFD();
+		m_face_net.~FaceNet();
+	}
 };
 
 EMSCRIPTEN_BINDINGS(AIbumCore) {
@@ -53,10 +76,16 @@ EMSCRIPTEN_BINDINGS(AIbumCore) {
 	    .constructor()
 	    .function("loadFromFile", &WASMImage::loadFromFile, e::allow_raw_pointers())
 	    .function("loadFromMemory", &WASMImage::loadFromMemory, e::allow_raw_pointers())
+	    .function("valid", &WASMImage::valid)
 	    .function("free", &WASMImage::free);
 
 	e::class_<WASMImageNet>("ImageNet")
 	    .constructor()
 	    .function("getTags", &WASMImageNet::getTags)
 	    .function("free", &WASMImageNet::free);
+
+	e::class_<WASMFaceNet>("FaceNet")
+		.constructor()
+		.function("getFaces", &WASMFaceNet::getFaces)
+		.function("free", &WASMFaceNet::free);
 }
